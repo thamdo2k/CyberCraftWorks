585e072081b3441bf72ecb8037fb373ca7f37530
thamdo2k <sofia2006tpun0suy@hotmail.com>
2024-05-11 12:12:02 +0700

    demo task

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-const person = { name: "Alice", age: 30 };
-if (person.hasOwnProperty("age")) {
-  console.log("The person has an age property");
-}
+const randomNumber = Math.floor(Math.random() * 10) + 1;
